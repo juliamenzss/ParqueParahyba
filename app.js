1f573124b350a9dece417c71e5429e23ab991277
Richelle Monteiro <114156215+richellemm@users.noreply.github.com>
2024-12-03 18:16:33 -0300

    Revert "Botão Mobile"

--- app.js
+++ app.js
@@ -8,16 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             navbar.classList.remove('hidden'); 
         }
-        lastScrollY = window.scrollY;
-        
-        document.addEventListener('DOMContentLoaded', () => {
-            const menuButton = document.querySelector('.mobile-menu-button');
-            const mobileMenu = document.querySelector('.mobile-menu');
-        
-            menuButton.addEventListener('click', () => {
-                mobileMenu.classList.toggle('active');
-            });
-        });
-        
+        lastScrollY = window.scrollY; 
     });
 });
